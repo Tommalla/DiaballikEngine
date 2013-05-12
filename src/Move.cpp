b5349a1e0c0bccf88912e5ff5044eb11e0c3af86
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-12 20:39:25 +0200

    Added Move class.

---
+++ src/Move.cpp
@@ -0,0 +1,6 @@
+/* Tomasz [Tommalla] Zakrzewski, 2013
+All rights reserved */
+
+#include "Move.h"
+
+
---
+++ src/Move.h
@@ -0,0 +1,14 @@
+/* Tomasz [Tommalla] Zakrzewski, 2013
+All rights reserved */
+
+#ifndef MOVE_H
+#define MOVE_H
+#include "Point.h"
+
+//It's a class because in future I might want to add some extra features to it
+class Move {
+	public:
+		Point from, to;
+};
+
+#endif // MOVE_H
