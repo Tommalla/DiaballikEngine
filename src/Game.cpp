5cf41612f8aead0a3bef4937def7cd04351e5e71
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-12 21:56:47 +0200

    Further Game implementation.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,87 @@
 /* Tomasz [Tommalla] Zakrzewski, 2013
 All rights reserved */
 
+#include <cassert>
 #include "Game.h"
+
+Game::Game() {
+	this->gameInProgress = false;
+	this->currentPlayer = NONE;
+}
+
+void Game::resetMoves() {
+	this->movesLeft = 2;
+	this->passesLeft = 1;
+}
+
+
+void Game::newGame() {
+	this->board = Board();
+	this->currentPlayer = NONE;
+	
+	this->resetMoves();
+	
+	this->gameInProgress = true;
+}
+
+bool Game::isMoveValid (const Point& from, const Point& to) const {
+	//TODO check if move is valid
+}
+
+bool Game::isMoveValid (const Move& move) const {
+	return this->isMoveValid(move.from, move.to);
+}
+
+void Game::makeMove (const Point& from, const Point& to) {
+	//TODO make move
+}
+
+void Game::makeMove (const Move& move) {
+	this->makeMove(move.from, move.to);
+}
+
+vector< Point > Game::getDestinationsFor (const int x, const int y) const {
+	//TODO get 'em!
+}
+
+vector< Point > Game::getDestinationsFor (const Point& pos) const {
+	return this->getDestinationsFor(pos.x, pos.y);
+}
+
+vector< Point > Game::getPiecesOf (const GamePlayer player) const {
+	assert(this->gameInProgress);
+	
+	vector<Point> res;
+	
+	for (int x = 0; x < BOARD_SIZE; ++x)
+		for (int y = 0; y < BOARD_SIZE; ++y)
+			if ((int)this->board.getFieldAt(x, y) == (int)player)
+				res.push_back(Point(x, y));
+			
+	return res;
+}
+
+bool Game::isFinished() {
+
+}
+
+GamePlayer Game::getWinner() const {
+	if (this->gameInProgress)
+		return NONE;
+	
+	if (this->illegalEnd)
+		return this->getOppositePlayer(this->currentPlayer);
+	
+	return this->currentPlayer;
+}
+
+GamePlayer Game::getOppositePlayer (const GamePlayer& player) const {
+	if (player == NONE)
+		return NONE;
+	return (player == GAME_PLAYER_A) ? GAME_PLAYER_B : GAME_PLAYER_A;
+}
+
+
+
+
 
--- src/Game.h
+++ src/Game.h
@@ -8,6 +8,7 @@ All rights reserved */
 #include "Move.h"
 //#include "Point.h"
 #include "enums.h"
+#include "constants.h"
 
 /**
  * @brief The main class of the engine
@@ -18,24 +19,39 @@ class Game {
 		Board board;
 		bool gameInProgress;
 		/**
+		 * @brief Indicates that the game finished after an illegal move
+		 **/
+		bool illegalEnd;
+		/**
 		 * @brief The amount of moves left for the current player
 		 **/
-		int movesLeft;
+		uint8_t movesLeft;
+		/**
+		 * @brief The amount of passes left for the current player
+		 **/
+		uint8_t passesLeft;
 		GamePlayer currentPlayer;
 		
+		void resetMoves();
+		
 	public:
+		Game();
 		void newGame();
 		
+		bool isMoveValid(const Point& from, const Point& to) const;
 		bool isMoveValid(const Move& move) const;
+		void makeMove(const Point& from, const Point& to);
 		void makeMove(const Move& move);
-		
-		bool isFinished();
-		GamePlayer getWinner() const;
 		
 		vector<Point> getDestinationsFor(const Point& pos) const;
 		vector<Point> getDestinationsFor(const int x, const int y) const;
 		
 		vector<Point> getPiecesOf(const GamePlayer player) const;
+		
+		bool isFinished();
+		GamePlayer getWinner() const;
+		
+		GamePlayer getOppositePlayer(const GamePlayer& player) const;
 };
 
 #endif // GAME_H
